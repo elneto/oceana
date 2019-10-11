4ce087a27d76406869668f664d14d3d421db5e55
Ernesto Araiza <Ernesto.Araiza@UNHQ.UN.ORG>
2019-10-11 14:46:39 -0400

    styled menu

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,43 +1,59 @@
 import React from "react"
 import { Link } from "gatsby"
-
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap"
-
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap"
 const CustomNavbar = ({ pageInfo }) => {
-  console.log(pageInfo)
+  //console.log(pageInfo)
   return (
     <>
-      <Navbar variant="dark" expand="lg" id="site-navbar">
-        {/* <Container> */}
-        <Link to="/" className="link-no-style">
-          <Navbar.Brand as="span">Gatsby React Bootstrap</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto" activeKey={pageInfo && pageInfo.pageName}>
-            <Link to="/page-2" className="link-no-style">
-              <Nav.Link as="span" eventKey="page-2">
-                Page 2
-              </Nav.Link>
-            </Link>
-          </Nav>
-          <Nav className="ml-auto">
-            <Form inline onSubmit={e => e.preventDefault()}>
-              <Form.Group>
-                <FormControl
-                  type="text"
-                  placeholder="Fake Search"
-                  className="mr-2"
-                />
-              </Form.Group>
-              <Button>Fake Button</Button>
-            </Form>
-          </Nav>
-        </Navbar.Collapse>
-        {/* </Container> */}
+      <Navbar variant="light" expand="lg" id="site-navbar">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto" activeKey={pageInfo && pageInfo.pageName}>
+              <Link to="/" className="link-no-style">
+                <Nav.Link as="span" eventKey="index">
+                  Home
+                </Nav.Link>
+              </Link>
+              <Link to="/page-2" className="link-no-style">
+                <Nav.Link as="span" eventKey="page-2">
+                  About
+                </Nav.Link>
+              </Link>
+              <Link to="/page-2" className="link-no-style">
+                <Nav.Link as="span" eventKey="page-2">
+                  Programme
+                </Nav.Link>
+              </Link>
+              <Link to="/page-2" className="link-no-style">
+                <Nav.Link as="span" eventKey="page-2">
+                  Documentation
+                </Nav.Link>
+              </Link>
+              <Link to="/page-2" className="link-no-style">
+                <Nav.Link as="span" eventKey="page-2">
+                  Participate
+                </Nav.Link>
+              </Link>
+              <Link to="/page-2" className="link-no-style">
+                <Nav.Link as="span" eventKey="page-2">
+                  News & Media
+                </Nav.Link>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   )
 }
-
 export default CustomNavbar
