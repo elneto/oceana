fc20fd56ac20826df41a34c764b5bc278ba0330a
neto <yo@ernestoaraiza.com>
2019-10-10 22:57:16 -0400

    added un logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from "../images/un-emblem.svg"
 
 const Header = ({ siteTitle }) => (
   <header
@@ -20,6 +21,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
+          <img id="logo" src={logo} alt="Emblem of the United Nations" />
           {siteTitle}
         </Link>
       </h1>
